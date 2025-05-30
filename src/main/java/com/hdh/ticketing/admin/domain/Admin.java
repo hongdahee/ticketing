a00ba27ab0be0798c7a1fd4eaad89e53ec595523
hongdahee <heewhy3513@gmail.com>
2025-05-30 15:21:53 +0900

    feat: oauth 설정

---
+++ src/main/java/com/hdh/ticketing/admin/domain/Admin.java
@@ -0,0 +1,12 @@
+package com.hdh.ticketing.admin.domain;
+
+import com.hdh.ticketing.user.domain.SiteUser;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin extends SiteUser {
+
+}
